6bc52194a469229d8e19f6e278b6738d652f77b6
Valerii2575 <valerii.osypenko@outlook.com>
2022-12-17 20:51:27 +0100

    Add DbContext

---
+++ src/BookReading.DomainLayer/Models/Entity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BookReading.DomainLayer.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+    }
+}
+
---
+++ src/BookReading.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BookReading.DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookReading.Infrastructure.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            // 1:N => Category : Books
+            builder.HasMany(c => c.Books)
+                .WithOne(b => b.Category)
+                .HasForeignKey(b => b.CategoryId);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
+
