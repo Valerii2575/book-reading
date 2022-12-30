18c13260968921065d7f7321ec4312c95374acc1
Valerij575 <valerij575@gmail.com>
2022-12-30 19:54:51 +0100

    Update repository in Infrastructure Layer

---
+++ src/BookReading.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookReading.DomainLayer.Interfaces;
+using BookReading.DomainLayer.Models;
+using BookReading.Infrastructure.Context;
+
+namespace BookReading.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BookReadingDbContext db) : base(db)
+        {
+        }
+    }
+}
